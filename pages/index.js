1cf80b902d874b65f02ca7c28f61da9db92ed9f9
spatulatom <spatulatom@gmail.com>
2022-12-03 22:13:04 +0000

    header

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ export default function Home() {
       </Head>
       <div>
         <h1 className={styles.title}>California Destinations</h1>
+        <h1 className={styles.title}>California Destinations</h1>
         <p className={styles.text}>Lorem ipsum ipsim dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
         <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
         <Link href="/hotels/">
